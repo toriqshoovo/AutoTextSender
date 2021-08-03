0bce28be5aac5ffa46539a0452e7f4ddc3e085aa
toriqshoovo <88261612+toriqshoovo@users.noreply.github.com>
2021-08-03 11:55:17 +0600

    Update Auto Text Sender.py

--- Auto Text Sender.py
+++ Auto Text Sender.py
@@ -1,7 +1,17 @@
 import pyautogui
 import time
+import random
 for i in range(0,100):
+    r = random.randint(0, 5)
+    n = random.randint(1,3)
+    dott = "." * r
     time.sleep(2)
-    pyautogui.typewrite("Write me here")
+    pyautogui.typewrite(f"Write me here{dott}")
     time.sleep(2)
     pyautogui.press("enter")
+    if i%n == 1:
+        r = random.randint(1, 4)
+        dott = "." * r
+        pyautogui.typewrite(f"I am here{dott}")
+        time.sleep(2)
+        pyautogui.press("enter")
